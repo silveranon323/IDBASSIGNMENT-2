04518b76b9286aae7b2afe14bd44dd4ced0f9c78
Hem Kishore Pradhan <hemkishoresoa@gmail.com>
2024-03-04 15:26:01 +0530

    Update ass2.sql

--- ass2.sql
+++ ass2.sql
@@ -16,3 +16,16 @@
 (h)select course_id from teachesf33 where semester='Spring';
 (i)select title from coursef33 where dept_name='Comp.Sci.' and credits<>3;
 (j)select * from instructorf33 order by dept_name desc;
+(k)update instructorf33 set date_of_join='22 June 2003' where id=12121
+(l)alter table instructorf35 date_of_join date;
+(m)alter table instructorf33 rename column dept_name to Department
+(n)alter table prereqf33 rename to prerequiredf33;			
+		 alter table coursef33 rename column course_id to Sub_code;
+	  	alter table prerequiredf33 rename column course_id to Sub_code
+			alter table prerequiredf33 rename column course_id to Sub_code
+(o)alter table coursef33 rename column course_id to Sub_code;
+(p)alter table instructorf33 modify name varchar2(50);
+(q)alter table instructorf33 rename to Faculty_Infof33;
+(r)alter table coursef33 modify Sub_Code varchar2(8);
+(s)drop table prereqf33 ;
+(t)alter table departmentf33 rename column building to builder
